ae1774c25dd35ac47238f6ba9ff048f758a2ead3
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-26 14:09:55 +0100

    Initial Commit

--- src/main/java/com/bjornspetprojects/microservices/limitsservice/controllers/LimitsConfigurationController.java
+++ src/main/java/com/bjornspetprojects/microservices/limitsservice/controllers/LimitsConfigurationController.java
@@ -1,14 +1,21 @@
 package com.bjornspetprojects.microservices.limitsservice.controllers;
 
 import com.bjornspetprojects.microservices.limitsservice.beans.LimitConfiguration;
+import com.bjornspetprojects.microservices.limitsservice.configuration.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    private final Configuration configuration;
+
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations(){
-        return new LimitConfiguration(99,9999);
+        return new LimitConfiguration(configuration.getMinimum(),configuration.getMaximum());
     }
 }
